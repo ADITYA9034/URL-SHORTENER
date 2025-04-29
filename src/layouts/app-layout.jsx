2e54d67ba9890b9231fbc93a650c83c4cb26bc64
ADITYA9034 <157782082+ADITYA9034@users.noreply.github.com>
2025-04-29 10:14:08 +0530

    first commit

---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+import Header from '@/components/header';
+
+const AppLayout = () => {
+  return <div>
+<main className='min-h-screen container'>
+<Header/>
+<Outlet/>
+</main>  
+<div className='p-10 text-center bg-gray-800 mt-10'>
+    Made By ADITYA & AKSHAY
+</div>
+</div>;
+};
+
+export default AppLayout
