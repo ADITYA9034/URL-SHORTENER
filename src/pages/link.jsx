fc60707cf290da538fdad23ecc8ec249f2cd6eb2
ADITYA9034 <157782082+ADITYA9034@users.noreply.github.com>
2025-04-29 10:19:24 +0530

    changed url

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -76,11 +76,11 @@ const LinkPage = () => {
             {url?.title}
           </span>
           <a
-            href={`https://trimmer.in/${link}`}
+            href={`https://url-shortener-xi-green.vercel.app/${link}`}
             target="_blank"
             className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
           >
-            https://trimmer.in/{link}
+            https://url-shortener-xi-green.vercel.app/{link}
           </a>
           <a
             href={url?.original_url}
@@ -97,7 +97,7 @@ const LinkPage = () => {
             <Button
               variant="ghost"
               onClick={() =>
-                navigator.clipboard.writeText(`https://trimmer.in/${link}`)
+                navigator.clipboard.writeText(`https://url-shortener-xi-green.vercel.app/${link}`)
               }
             >
               <Copy />
